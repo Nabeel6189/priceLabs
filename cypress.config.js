d579634ed1d1a9a95b1beb16af0a3ffa489075de
Nabeel6189 <nabeel13b@gmail.com>
2024-08-07 22:17:19 +0530

    added reports configuration

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,20 @@ module.exports = defineConfig({
   e2e: {
     testIsolation: false,
     baseUrl:"https://app.pricelabs.co",
+    reporter: "cypress-mochawesome-reporter",
     setupNodeEvents(on, config) {
-      // implement node event listeners here 
+      // implement node event listeners here
+      screenshotOnRunFailure:true 
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
+  {
+    reporter: "mochawesome",
+    reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true,
+    }
+},
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,7 +15,9 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-mochawesome-reporter/register'
 require('cypress-xpath');
+
 
 
 // Alternatively you can use CommonJS syntax:
